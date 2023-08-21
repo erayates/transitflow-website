90c97b6c4fb458d29cbb96de51daeda51add7a5a
Eray Ates <72857634+erayates@users.noreply.github.com>
2023-08-22 02:23:37 +0300

    Why us section created and completed.

---
+++ src/containers/home-container/what-we-do/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styles from "./style.module.css";
+import Image from "next/image";
+
+function WhatWeDo() {
+  return (
+    <section id="#what-we-do" className={styles.container}>
+      <div className={styles.whatWeDoHeader}>
+        <p className={styles.subtitle}>What We Do</p>
+        <h2 className={styles.title}>Safe & Reliable Cargo Solutions</h2>
+      </div>
+      <div className={styles.whatWeDoItem}>
+        <div className={styles.itemInnerDiv}>
+          <Image src="/assets/icons/sea-transport.svg" width={50} height={50} />
+          <div className={styles.itemInfo}>
+            <h3 className={styles.itemTitle}>Sea Transport Services</h3>
+            <p className={styles.itemDescription}>
+              Following the quality of our service thus having gained trust of
+              our many clients.
+            </p>
+          </div>
+        </div>
+        <div className={styles.itemInnerDiv}>
+          <Image src="/assets/icons/air-fright.svg" width={50} height={50} />
+          <div className={styles.itemInfo}>
+            <h3 className={styles.itemTitle}>Air Fright Services</h3>
+            <p className={styles.itemDescription}>
+              Following the quality of our service thus having gained trust of
+              our many clients.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.whatWeDoItem}>
+        <div className={styles.itemInnerDiv}>
+          <Image src="/assets/icons/warehousing.svg" width={50} height={50} />
+          <div className={styles.itemInfo}>
+            <h3 className={styles.itemTitle}>Warehousing Services</h3>
+            <p className={styles.itemDescription}>
+              Following the quality of our service thus having gained trust of
+              our many clients.
+            </p>
+          </div>
+        </div>
+        <div div className={styles.itemInnerDiv}>
+          <Image
+            src="/assets/icons/local-shipping.svg"
+            width={50}
+            height={50}
+          />
+          <div className={styles.itemInfo}>
+            <h3 className={styles.itemTitle}>Air Fright Services</h3>
+            <p className={styles.itemDescription}>
+              Following the quality of our service thus having gained trust of
+              our many clients.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default WhatWeDo;
---
+++ src/containers/home-container/why-us/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styles from "./style.module.css";
+import Image from "next/image";
+
+function WhyUs() {
+  return (
+    <section id="#why-us">
+      <div className={styles.banner}>
+        <Image src="/assets/images/why-us-banner.jpg" fill objectFit="cover" />
+        <div className={styles.bannerGradient} />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.containerLeft}>
+          <p className={styles.subtitle}>Why Us</p>
+          <h2 className={styles.title}>
+            We provide full range global logistics solution
+          </h2>
+          <p className={styles.description}>
+            Leverage agile frameworks to provide a robust synopsis for strategy
+            foster collaborative thinking to further the overall value
+            proposition.
+          </p>
+          <p className={styles.description}>
+            Organically grow the holistic world view of disruptive innovation
+            via workplace diversity and empowerment.
+          </p>
+          <div className={styles.features}>
+            <div className={styles.feature}>
+              <Image
+                src="/assets/icons/delivery-on-time.svg"
+                alt="Delivery On Time Icon"
+                width={50}
+                height={50}
+              />
+              <p>Delivery On Time</p>
+            </div>
+            <div className={styles.feature}>
+              <Image
+                src="/assets/icons/travel-cost.svg"
+                alt="Optimized Travel Cost Icon"
+                width={50}
+                height={50}
+              />
+              <p>Optimized Travel Cost</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.containerRight}>
+          <Image
+            src="/assets/images/why-us-image.jpg"
+            alt="Why Us Image"
+            width={600}
+            height={350}
+          />
+          <Image
+            src="/assets/images/why-us-image-small.jpg"
+            alt="Why Us Image Small"
+            width={320}
+            height={250}
+            objectFit="cover"
+            className={styles.imageSmall}
+          />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.innerContainer}>
+          <div>
+            <p>1294</p>
+            <div className={styles.square} />
+            <span>Delivered Packages</span>
+          </div>
+          <div className={styles.divider}></div>
+          <div>
+            <p>1294</p>
+            <div className={styles.square} />
+            <span>Delivered Packages</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default WhyUs;
